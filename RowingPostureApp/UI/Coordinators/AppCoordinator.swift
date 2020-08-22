d621eecf4b2c6c814f59734c89c5f712d7d71125
Kynan <k.song@blueyonder.co.uk>
2020-08-22 07:49:41 +0100

    Adding to the app coordinator

--- RowingPostureApp/UI/Coordinators/AppCoordinator.swift
+++ RowingPostureApp/UI/Coordinators/AppCoordinator.swift
@@ -13,5 +13,52 @@ enum AppTransitions: TransitionRef {
 }
 
 class AppCoordinator: BaseCoordinator {
+
+    weak var parentCoordinator: BaseCoordinator?
+    var childCoordinator: BaseCoordinator?
+    var rootViewController: UIViewController
+    var navController: UINavigationController
     
+    var overlayWindow: UIWindow
+    
+    //var mainOptionsCoordinator: MainOptionsCoordinator?
+    
+    required init() {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.isHidden = true
+        self.rootViewController = navController
+    }
+    
+    func reset() {
+        //
+    }
+    
+    @discardableResult func transition(to page: TransitionRef, object: Any?) -> Bool {
+        guard let page = page as? AppTransitions else {
+            return false
+        }
+        
+        switch page {
+        case .MainOptions:
+            presentMainOptions()
+        }
+        return true
+    }
+    
+    func dismiss(_ completion: (() -> Void)?) {
+        //
+    }
+    
+    func dismiss() {
+        //
+    }
+}
+
+extension AppCoordinator {
+    
+    func presentMainOptions() {
+        print("Main options page")
+    }
 }

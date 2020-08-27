9fc726a9d915215814ddca8f76c9857c69d79934
Kynan <k.song@blueyonder.co.uk>
2020-08-27 16:33:18 +0100

    Adding folder for bluetooth central manager for PM5 syncing

---
+++ RowingPostureApp/Bluetooth/BluetoothHelper.swift
@@ -0,0 +1,55 @@
+//
+//  BluetoothHelper.swift
+//  RowingPostureApp
+//
+//  Created by Kynan Song on 27/08/2020.
+//  Copyright © 2020 xDesign. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+protocol BluetoothInterface {
+    func scanForDevices()
+    func stopScan()
+    func disconnect()
+}
+
+class BluetoothHelper: NSObject, BluetoothInterface {
+    
+    var manager: CBCentralManager?
+    
+    var didUpdateCharacteristics:((CBCharacteristic?, CBPeripheral?) ->())?
+    
+    override init() {
+        super.init()
+        
+        if manager == nil {
+            manager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+        }
+    }
+    
+    func scanForDevices() {
+        //
+    }
+    
+    func stopScan() {
+        //
+    }
+    
+    func disconnect() {
+        //
+    }
+    
+    
+}
+
+extension BluetoothHelper: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        // Need to read PM5 docs.
+    }
+    
+    
+}
+
+//https://www.raywenderlich.com/231-core-bluetooth-tutorial-for-ios-heart-rate-monitor

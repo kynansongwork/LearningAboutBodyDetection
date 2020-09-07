31818cd3a152e96427753206072f86fbba80636b
Kynan <k.song@blueyonder.co.uk>
2020-09-07 16:12:32 +0100

    Working through and making notes on the PoseImageView

--- RowingPostureApp/UI/View Controllers/Video Capture/PoseImageView.swift
+++ RowingPostureApp/UI/View Controllers/Video Capture/PoseImageView.swift
@@ -23,7 +23,6 @@ class PoseImageView: UIImageView {
     // joints to create a fully rendered 'pose'.
     
     static let jointSegments = [
-        
         JointSegment(jointA: .leftHip, jointB: .leftShoulder),
         JointSegment(jointA: .leftShoulder, jointB: .leftElbow),
         JointSegment(jointA: .leftElbow, jointB: .leftWrist),
@@ -37,4 +36,86 @@ class PoseImageView: UIImageView {
         JointSegment(jointA: .leftShoulder, jointB: .rightShoulder),
         JointSegment(jointA: .leftHip, jointB: .rightHip)
     ]
+    
+    // Setting up how the connecting lines between joints will look.
+    
+    @IBInspectable var segmentLineWidth: CGFloat = 2
+    @IBInspectable var segmentColour: UIColor = UIColor.systemTeal
+    
+    // Circles at the joint points.
+    
+    @IBInspectable var jointRadius: CGFloat = 4
+    @IBInspectable var jointColour: UIColor = UIColor.systemRed
+    
+    // This method is used to render the image of the pose.
+    // It takes an array of Poses that have been detected, and a frame is the image that has been used
+    // to detect the poses. It is then used as the background of the returned image that contains the rendered poses.
+    
+    func show(poses: [Pose], on frame: CGImage) {
+        
+        let imageSize = CGSize(width: frame.width, height: frame.height)
+        
+        //  Determines the attributes of the core graphics context created by the image renderer.
+        let imageFormat = UIGraphicsImageRendererFormat()
+        imageFormat.scale = 1
+        
+        let renderer = UIGraphicsImageRenderer(size: imageSize, format: imageFormat)
+        
+        let renderedImage = renderer.image { rendererContext in
+            
+            // The current frame is used as the background for the newly drawn image of the poses.
+            drawPoses(image: frame, in: rendererContext.cgContext)
+            
+            // A loop is used to access each joined segment to draw the lines between them.
+            for pose in poses {
+                for segment in PoseImageView.jointSegments {
+                    let jointA = pose[segment.jointA]
+                    let jointB = pose[segment.jointB]
+                    
+                    guard jointA.isValid, jointB.isValid else {
+                        
+                        // continue is used to direct the for loop to start again, but from the
+                        // beginning of the next iteration of the loop. Progresses the loop incrementally.
+                        continue
+                    }
+                    
+                    drawLine(from: jointA, to: jointB, in: rendererContext.cgContext)
+                }
+                
+                // Loop through the valid joints and draw circles above the segment lines where necessary.
+                for joint in pose.joints.values.filter( { $0.isValid } ) {
+                    drawCircles(circle: joint, in: rendererContext.cgContext)
+                }
+            }
+        }
+        
+        image = renderedImage
+    }
+    
+    // This method will flip and then draw the given image.
+    // The arguments taken are the image that will be drawn onto the context and the rendering context.
+    func drawPoses(image: CGImage, in cgContext: CGContext) {
+        
+        // Created and pushes a copy of the current graphic state onto the stack to provide context.
+        cgContext.saveGState()
+        
+        // The context will be flipped prior to image rendering as the image passed in will be assumed to be flipped.
+        cgContext.scaleBy(x: 1.0, y: -1.0)
+        
+        // Next the rendered image is adjusted for the scale changes made.
+        let drawingRect = CGRect(x: 0, y: -image.height, width: image.width, height: image.height)
+        cgContext.draw(image, in: drawingRect)
+        
+        // Sets the current graphics state to the most recently saved.
+        cgContext.restoreGState()
+    }
+    
+    func drawCircles(circle joint: Joint, in cgContext: CGContext) {
+        
+    }
+    
+    // Method to render the connection between joint pairs.
+    func drawLine(from parentJoint: Joint, to childJoint: Joint, in cgContext: CGContext) {
+        
+    }
 }
